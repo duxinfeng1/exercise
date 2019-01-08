a5b53b994fd8f8abe10517bdded93233824e2560
杜鑫峰 <2687538782@qq.com>
2019-01-08 18:51:19 +0800

    java补上

--- src/main/java/com/dfz/controller/GradeController.java
+++ src/main/java/com/dfz/controller/GradeController.java
@@ -2,11 +2,14 @@ package com.dfz.controller;
 
 import com.dfz.service.IGradeService;
 import com.dfz.vo.ResultVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(produces = "这是Java编写的接口文档",value = "接口文档")
 @RestController
 @CrossOrigin
 public class GradeController {
@@ -14,6 +17,7 @@ public class GradeController {
     @Autowired
     private IGradeService gradeS;
 
+    @ApiOperation(notes = "实现传递参数，响应指定数量的数据",tags = {"数量","名次"},value = "获取名次接口")
     @GetMapping("/grade.do")
     public ResultVo findAll() {
         ResultVo vo = gradeS.findAll();
